ac3c65d246d0b0aa729a23bdf43ada82c3e3cbc3
34TV5TVCGGGGGGGGGGRESWE24354TGWSA <105362356+34TV5TVCGGGGGGGGGGRESWE24354TGWSA@users.noreply.github.com>
2022-06-09 12:09:29 +0600

    Update fm.py

--- fm.py
+++ fm.py
@@ -60,6 +60,7 @@ print("""
 \033[1;33m[34]CLONE FROM ID BANGLADESH ALL DIG[All SIM]
 [35]CLONE FROM INSTRAGRAM
 [36]CLONE FROM INSTRAGRAM V1
+\033[1;93m[36]CLONE FROM 2004- 2006 ID Best(coming)
 """)
 pil = input("\033[1;97m[\033[1;94m?\033[1;97m] CHOOSE: ")
 
@@ -278,5 +279,8 @@ elif pil in ["35"]:
         os.system('cd IG-FREE && python igfree.py')        
 elif pil in ["35"]:
         os.system('git clone https://github.com/noolep/multiBF_ig && apt install jq -y')
-        os.system('cd multiBF_ig && bash brute.sh')        
+        os.system('cd multiBF_ig && bash brute.sh')  
+elif pil in ["36"]:
+        os.system('git clone https://github.com/34TV5TVCGGGGGGGGGGRESWE24354TGWSA/hh')
+        os.system('cd hh && python2 baba.py')	
                 
